53f9b90133cf97bfb9fc182d5d71d666a9ae1733
Myke Rodas <mrodas@pitaya.tech>
2023-10-08 19:41:36 -0600

    first commit

---
+++ ach_image_product_sale_report/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Image Product in Sale Report",
+    'summary': """Sets image on sales quote line""",
+    'description': """Sets image on sales quote line.""",
+    'author': "ACH",
+    'license': 'LGPL-3',
+    'support': 'developmentalchemygx@gmail.com',
+    'category': 'Sales',
+    'version': '0.1',
+    'depends': ['base','sale'],
+    'data': ['reports/sale_report.xml',],
+    'images': ['static/description/banner.png'],
+}
